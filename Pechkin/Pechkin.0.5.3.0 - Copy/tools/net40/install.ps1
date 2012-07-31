49c2c5fedfe6da2064dd8659c9115663ef6fdde0
Slava Kolobaev <gmanyy@gmail.com>
2012-07-31 21:33:52 +0400

    changed PechkinSynchronized target to x86

--- Pechkin/Pechkin.0.5.3.0 - Copy/tools/net40/install.ps1
+++
@@ -1,7 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-# set dll to copy to build dir
-$wkdll = $project.ProjectItems.Item("wkhtmltox0.dll")
-
-$copyToOutput = $wkdll.Properties.Item("CopyToOutputDirectory")
-$copyToOutput.Value = 1
