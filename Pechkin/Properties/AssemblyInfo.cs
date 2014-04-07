c475209dc2981119282993346507e2fb7ac3e89c
tuespetre <dcgray@yeahbaby.fake>
2014-04-07 15:47:16 -0500

    Changes
    Shifted the unmanaged assemblies a little bit. Changed assembly name and
    version and made a nuget package.

--- Pechkin/Properties/AssemblyInfo.cs
+++ Pechkin/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pechkin")]
+[assembly: AssemblyTitle("TuesPechkin")]
 [assembly: AssemblyDescription(".NET Wrapper for WkHtmlToPdf static DLL. Allows you to utilize full power of the library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pechkin")]
-[assembly: AssemblyCopyright("Copyright 2012 Slava Kolobaev")]
+[assembly: AssemblyProduct("TuesPechkin")]
+[assembly: AssemblyCopyright("Copyright 2014 Derek Gray")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.8.1")]
-[assembly: AssemblyFileVersion("0.5.8.1")]
+[assembly: AssemblyVersion("0.9")]
+[assembly: AssemblyFileVersion("0.9")]
